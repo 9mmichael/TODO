b0d7cb30a598d20b607cc583b4833c912ea59efa
9mmichael <ueno.michael@gmail.com>
2019-03-06 01:26:20 +0900

    Change how to save to JSON

--- app/src/main/java/com/qmmichael/todo/ui/main/MainFragment.kt
+++ app/src/main/java/com/qmmichael/todo/ui/main/MainFragment.kt
@@ -1,6 +1,8 @@
 package com.qmmichael.todo.ui.main
 
+import android.content.Context
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -15,6 +17,10 @@ import android.widget.TextView
 import com.qmmichael.todo.R
 import com.qmmichael.todo.data.model.TodoItem
 import com.qmmichael.todo.util.FrequentRegax
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import org.json.JSONArray
+import org.json.JSONException
 
 /**
  * Created by 9mmichael on 2019/02/16.
@@ -34,23 +40,21 @@ class MainFragment : Fragment() {
 
     val mainRecyclerView: RecyclerView? = view?.findViewById(R.id.main_recycler_view)
     val mainEditText: EditText? = view?.findViewById(R.id.edit_text)
-    val mutableList =  MutableList(1,{ TodoItem(false, "hoge") })
+    val mutableList : MutableList<TodoItem> = loadList()
 
     mainRecyclerView?.let {
       it.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
     }
 
-    mutableList.let {
-      mainRecyclerView?.adapter = MainAdapter(it)
-    }
+    mainRecyclerView?.adapter = MainAdapter(mutableList)
 
     mainEditText?.let { editText ->
       editText.setOnKeyListener(View.OnKeyListener(fun (view: View, keyCode: Int, event: KeyEvent) : Boolean {
         if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER && editText.text.toString() != "") {
-          mutableList.let { mutableList ->
-            if (!FrequentRegax.DESCRIPTION.isMatched(editText.text.toString())) {
-              mutableList.add(TodoItem(false, editText.text.toString()))
-            }
+          if (!FrequentRegax.DESCRIPTION.isMatched(editText.text.toString())) {
+            mutableList.add(TodoItem(false, editText.text.toString()))
+            saveList(mutableList, context)
+            mainRecyclerView?.adapter = MainAdapter(loadList())
           }
           editText.text.clear()
           return true
@@ -59,6 +63,54 @@ class MainFragment : Fragment() {
       })
       )
     }
+  }
+
+  fun saveList(mutableList: MutableList<TodoItem>, context: Context?) {
+    val type = Types.newParameterizedType(MutableList::class.java, TodoItem::class.java)
+    val jsonAdapter = Moshi.Builder().build().adapter<MutableList<TodoItem>>(type)
+    context?.let {
+      PreferenceManager.getDefaultSharedPreferences(it)
+          .edit()
+          .putString("todo_list", jsonAdapter.toJson(mutableList))
+          .apply()
+    }
+  }
+
+  fun loadList() : MutableList<TodoItem> {
+    val mutableList : MutableList<TodoItem> = mutableListOf()
+    val listJson = PreferenceManager.getDefaultSharedPreferences(context)
+        .getString("todo_list", "")
+    if (listJson == "[]" || FrequentRegax.INCLUDE_NULL.isMatched(listJson)) {
+      return initializeList()
+    }
+    else {
+      try {
+        val jsonArray = JSONArray(listJson)
+        val type = Types.newParameterizedType(MutableList::class.java, TodoItem::class.java)
+        val jsonAdapter = Moshi.Builder().build().adapter<MutableList<TodoItem>>(type)
+        val todoItemList = jsonAdapter.fromJson(listJson)
+        listJson?.let {
+          todoItemList?.let {
+            for (i in 0..(jsonArray.length()-1)) {
+              mutableList.add(TodoItem(it[i].isChecked, it[i].itemText))
+            }
+          }
+        }
+      }
+      catch (e: JSONException) {
+      }
+    }
+    return mutableList
+  }
+
+  private fun initializeList() : MutableList<TodoItem> {
+    val mutableList : MutableList<TodoItem> = mutableListOf()
+    saveList(mutableList, context)
+    return mutableList
+  }
+
+  companion object {
+    fun newInstance() = MainFragment()
   }
 }
 
--- app/src/main/java/com/qmmichael/todo/util/FrequentRegax.kt
+++ app/src/main/java/com/qmmichael/todo/util/FrequentRegax.kt
@@ -3,7 +3,8 @@ package com.qmmichael.todo.util
 import java.util.regex.Pattern
 
 enum class FrequentRegax(val value: String) {
-  DESCRIPTION("\n|\\s");
+  DESCRIPTION("\n|\\s"),
+  INCLUDE_NULL(".*null.*");
 
   fun isMatched(value: String?): Boolean {
     return Pattern.compile(this.value, Pattern.DOTALL).matcher(value).matches()
